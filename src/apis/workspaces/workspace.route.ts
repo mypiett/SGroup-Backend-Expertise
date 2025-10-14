3b8fd1cce5d744bf66117eb176d4e191fdae7978
thanhtuyen <thanhtuyen27122005@gmail.com>
2025-10-14 14:55:05 +0700

    feat: CRUD workspace

--- src/apis/workspaces/workspace.route.ts
+++ src/apis/workspaces/workspace.route.ts
@@ -4,5 +4,8 @@ import { WorkspaceController } from './workspace.controller';
 const route = Router();
 
 route.post('/create', WorkspaceController.createWorkspace);
-
+route.get('/', WorkspaceController.getAllWorkspaces);
+route.get('/:id', WorkspaceController.getWorkspaceById);
+route.put('/:id', WorkspaceController.updateWorkspace);
+route.delete('/:id', WorkspaceController.deleteWorkspace);
 export default route;
