aef73a7e47e9c97df30f0f6b7dc31f546f26a921
thanhtuyen <thanhtuyen27122005@gmail.com>
2025-10-18 23:58:10 +0700

    feat: CRUD board

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import UserRouter from './users/user.route';
 import AuthRouter from './auth/auth.route';
 import WorkspaceRouter from './workspaces/workspace.route';
+import BoardRouter from './boards/board.route';
 import authenticateJWT from '../common/middleware/authentication';
 
 const route = Router();
 route.use('/users', authenticateJWT, UserRouter);
 route.use('/auth', AuthRouter);
 route.use('/workspace', authenticateJWT, WorkspaceRouter);
+route.use('/board', authenticateJWT, BoardRouter);
 export default route;
