42904a748ee7daac7a46b0a0f6246fa1a4d4c8f8
Nguyễn Hồng Phúc <119803856+YomostCodeDao@users.noreply.github.com>
2025-11-17 22:27:24 +0700

    Refactor upload middleware for file storage

--- src/common/middleware/upload.middleware.ts
+++ src/common/middleware/upload.middleware.ts
@@ -1,16 +1,14 @@
 import multer from 'multer';
 
-// Lưu trữ file vào thư mục "uploads/avatars"
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/avatars/');
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname); // Đặt tên file theo thời gian
+        cb(null, Date.now() + '-' + file.originalname); 
     },
 });
 
-// Chỉ cho phép upload ảnh có định dạng .jpg, .png, .jpeg
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
         cb(null, true);
