902442f5be58bce5125adb343a6aabba6407ac64
Viettrung <nguyenviettrung2005@gmail.com>
2025-12-05 15:27:43 +0700

    feat: add api lists (basic)

--- src/common/entities/list.entity.ts
+++ src/common/entities/list.entity.ts
@@ -1,6 +1,7 @@
 import {
   Column,
   Entity,
+  Index,
   JoinColumn,
   ManyToOne,
   OneToMany,
@@ -12,6 +13,10 @@ import { Board } from './board.entity';
 import { Card } from './card.entity';
 
 @Entity('lists')
+@Index('idx_list_board_id', ['board'])
+@Index('idx_list_archived', ['isArchived'])
+@Index('idx_list_board_position', ['board', 'position'])
+@Index('idx_list_board_archived', ['board', 'isArchived'])
 export class List extends DateTimeEntity {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
@@ -19,8 +24,11 @@ export class List extends DateTimeEntity {
   @Column({ type: 'varchar', length: 255 })
   title: string;
 
-  @Column({ type: 'int', default: 0 })
+  @Column({ type: 'float', default: 0 })
   position: number;
+
+  @Column({ type: 'boolean', default: false })
+  isArchived: boolean;
 
   @ManyToOne(() => Board, (board) => board.id, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'boardId' })
